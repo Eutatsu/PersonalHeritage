e6371ff62e0cb2588315d56b14deb94a8d45bd8a
Eutatsu <eudald.inventor@gmail.com>
2026-01-02 13:06:14 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react'
+import m1 from './assets/M1.svg'
+import m2 from './assets/M2.svg'
+import m3 from './assets/M3.svg'
+
+function App() {
+   const [percentage, setHeight] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      // Example: max height is 100vh, min is 50px
+      const newHeight = Math.min(200, scrollY/7 ); // Adjust formula as needed
+      console.log(scrollY, newHeight);
+      setHeight(newHeight);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+
+  return (
+    <>
+        <h1 className="text-3xl z-1 fixed top-[20vh] px-12">Taking in the View</h1>
+        <div className="top-[230vh] absolute w-full flex flex-col items-center">
+
+          <div className="relative hover:mb-40 mb-[-4rem] h-40 w-full z-1 transition-all ease-in-out justify-center" style={{ filter: 'drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.4))' }}>
+            <img src={m1} alt="M1" className='mb-[-5px]'/>
+            <div className="absolute px-2  w-full mx-auto mt-[-1rem]">
+              <h2 className='text-lg'>Creativity</h2>
+              </div>
+            <div className="bg-base h-80"></div>
+          </div>
+
+          <div className=" hover:mb-40 mb-[-4rem] h-40 w-full drop-shadow-lg z-1 transition-all ease-in-out">
+            <img src={m2} alt="M2" className='mb-[-5px]'/>
+            <div className="absolute px-2 max-w-screen-md w-full mx-auto mt-[-1rem]">
+              <h2 className='text-lg'>Rationality</h2>
+              </div>
+            <div className="bg-mid h-80"></div>
+          </div>
+
+          <div className=" hover:mb-40 mb-[-4rem] h-40 w-full drop-shadow-lg z-1 text-white transition-all ease-in-out">
+            <img src={m3} alt="M3" className='mb-[-5px]'/>
+            <div className="absolute px-2 max-w-screen-md w-full mx-auto mt-[-1rem]">
+              <h2 className='text-lg'>Nature</h2>
+              </div>
+            <div className="bg-full h-80"></div>
+          </div>
+        </div>
+       
+        <div style={{ minHeight: '100vh' }}>
+  {/* Your content */}
+</div>
+       <div className="hidden">
+         { Array.from({ length: 100 }).map((_, i )=>
+         <div key={i} className="fixed top-0 border-l border-mid z-0"
+          style={{ height: `${percentage-i}vh`, left: `${(i+1)*20}px` , opacity:`${percentage/300}`}}>
+         
+          </div>
+               
+               )}
+         { Array.from({ length: 100 }).map((_, i )=>
+         <div  key={i} className="fixed  left-0  border-t border-mid  z-0"
+          style={{ width: `${percentage-i}vw`, top: `${(i+1)*20}px`, opacity:`${percentage/300}` }}>
+         
+          </div>
+               
+               )}
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: '#ecede1',
+        mid: '#a6b49b',
+        full: '#515d37',
+        accent: '#987bb6',
+      },
+      borderColor: {
+        base: '#ecede1',
+        mid: '#a6b49b',
+        full: '#515d37',
+        accent: '#987bb6',
+      },
+    },
+  },
+  plugins: [],
+}
